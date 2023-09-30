7fa86dd2af4ea87ec2a6e060874ed446cd5c5d05
syw(laptop) <syw9846@naver.com>
2023-09-30 23:09:56 +0900

    Follow the mouse

--- drill06.py
+++ drill06.py
@@ -7,6 +7,18 @@ tuk_ground = load_image('TUK_GROUND.png')
 daco_character = load_image('Daco.png')
 hand_character = load_image('hand_arrow.png')
 
+def handle_events():
+    global running
+    global x2, y2
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEMOTION:
+            x2, y2 = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+    pass
 
 def tuk_canvas():
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
@@ -33,17 +45,18 @@ def draw_line():
     if distance <= 50:
         x2,y2 =random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
 
-
+running = True
 tuk_canvas()
 frame = 0
 x1 ,y1 = TUK_WIDTH // 2 , TUK_HEIGHT // 2
 x2,y2 = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
 right, left = False , False
 
-while True:
+while running:
     clear_canvas()
     tuk_canvas()
     rightleft(x1, x2)
+    handle_events()
 
     if right:
         daco_character.clip_draw(frame * 121, 122, 121, 122, x1, y1, 120, 120)
