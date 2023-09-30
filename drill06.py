836fba9138eee06d9addc36324c110ae019e2201
syw(laptop) <syw9846@naver.com>
2023-09-30 23:35:42 +0900

    The character goes well in the mouse click position

--- drill06.py
+++ drill06.py
@@ -10,12 +10,19 @@ hand_character = load_image('hand_arrow.png')
 def handle_events():
     global running
     global x2, y2
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
+        # elif event.type == SDL_MOUSEMOTION:
+        #     x2, y2 = event.x, TUK_HEIGHT - 1 - event.y
+
+        elif event.type == SDL_MOUSEBUTTONDOWN:
             x2, y2 = event.x, TUK_HEIGHT - 1 - event.y
+            if event.key == SDL_BUTTON_LEFT:
+                hand_character.draw(x2, y2)
+
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
@@ -23,7 +30,7 @@ def handle_events():
 def tuk_canvas():
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
 
-def rightleft(x1,x2):
+def rightleft(x1, x2):
     global left,right
 
     if x2 > x1:
@@ -41,9 +48,9 @@ def draw_line():
     x1 = (1 - 0.05) * x1 + 0.05 * x2
     y1 = (1 - 0.05) * y1 + 0.05 * y2
 
-    distance = (x2 -x1)**2 +(y2-y1)**2
+    distance = (x2 - x1)**2 + (y2 - y1)**2
     if distance <= 50:
-        x2,y2 =random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
+        x2, y2 = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
 
 running = True
 tuk_canvas()
@@ -51,19 +58,20 @@ frame = 0
 x1 ,y1 = TUK_WIDTH // 2 , TUK_HEIGHT // 2
 x2,y2 = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
 right, left = False , False
+# hide_cursor()
 
 while running:
     clear_canvas()
     tuk_canvas()
     rightleft(x1, x2)
     handle_events()
+    hand_character.draw(x2, y2)
 
     if right:
         daco_character.clip_draw(frame * 121, 122, 121, 122, x1, y1, 120, 120)
     elif left:
         daco_character.clip_draw(frame * 121, 0, 121, 122, x1, y1, 120, 120)
 
-    hand_character.draw(x2,y2)
 
     frame = (frame + 1) % 9
     delay(0.02)
